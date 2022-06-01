3186b6aa44a907d34355d512e6e4bb36405d4719
Micaela Giorgio <micaela.daiana.giorgio@gmail.com>
2022-06-01 02:05:22 -0300

    modifique el archivo adoption en la parte de genero; genere el evento de guardado en local storage del archivo js

--- script/adoption.js
+++ script/adoption.js
@@ -1,4 +1,27 @@
-window.addEventListener('DOMContetLoaded', () =>{
+window.addEventListener('DOMContentLoaded', () =>{
 
-    const form = document.querySelector('form')
+    const myForm = document.querySelector('form')
+    const inputName = myForm.querySelector('#name');   
+    const inputLastName = myForm.querySelector('#lastName');   
+    const inputEmail = myForm.querySelector('#email');
+    const inputTelephone = myForm.querySelector('#telephone');
+    const inputKittens = myForm.querySelector('#gender');
+    const inputAge = myForm.querySelector('#age');
+    const submitButton = myForm.querySelector('#submit');
+
+    submitButton.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        const adopterInformation ={
+            name: inputName.value,
+            lastName: inputLastName.value,
+            email: inputEmail.value,
+            telephone: inputTelephone.value,
+            kittens: inputKittens.value,
+            age: inputAge.value,
+        }
+
+        localStorage.setItem('adopterInformation', JSON.stringify(adopterInformation));
+    })
+
 })
